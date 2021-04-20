6910632e890c6df07729951ed7a3bcebe32adce2
dcota <duartenunocota@gmail.com>
2021-04-20 17:52:39 +0000

    update

--- TesteJavaMySQL_T2/src/main/MYSQLConnection.java
+++ TesteJavaMySQL_T2/src/main/MYSQLConnection.java
@@ -11,7 +11,7 @@ public class MYSQLConnection {
     //atributos
     private Properties p;
     private Connection connection;
-    //construtor
+    //construtor que cria a ligação à base de dados
     public MYSQLConnection() throws IOException {
         p = new Properties();
         try {
@@ -25,7 +25,7 @@ public class MYSQLConnection {
             System.out.println("Ocorreu um erro...");
         }
     }
-
+    //método para consultar uma tabela na base de dados
     public ResultSet querySELECT (String sql) {
         try{
             Statement statement = connection.createStatement();
@@ -36,9 +36,58 @@ public class MYSQLConnection {
         }
         return null;
     }
+    //inserir uma linha na tabela city
+    public void insertLineCity(String nome, String codigo, String distrito, int popul){
+        try {
+            //preprar a inserção da nova linha
+            String sql = "INSERT INTO city (Name, CountryCode, District, Population) VALUES (?,?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1,nome);
+            statement.setString(2,codigo);
+            statement.setString(3,distrito);
+            statement.setInt(4,popul);
+            //executar a inserção
+            int linhas = statement.executeUpdate();
+            if(linhas == 1){
+                System.out.println("Linha adicionada com sucesso!");
+            }
 
-    public void insertLine(){
+        } catch (SQLException e){
+            System.out.println("Ocorreu um erro ao inserir a linha...");
+        }
+    }
+    //eliminar uma linha da tabela city
+    public void deleteFromCity(int ID){
+        try {
+            String sql = "DELETE FROM city WHERE ID=" + ID;
+            Statement statement = connection.createStatement();
+            int linhas = statement.executeUpdate(sql);
+            if(linhas == 1){
+                System.out.println("Linha eliminada com sucesso");
+            } else if (linhas == 0) {
+                System.out.println("ID não encontrado");
+            }
+        } catch (SQLException e){
+            System.out.println("Ocorreu um erro na eliminação da linha...");
+        }
+    }
+
+    //calcular a média de população de um país
+    public void mediaPopul() throws SQLException {
+        String sql = "SELECT Name, Population FROM city WHERE CountryCode = 'PRT'";
+        ResultSet result = querySELECT(sql);
+        int somaPop = 0;
+        int cont = 0;
+        while(result.next()){
+            somaPop += result.getInt(2);
+            cont++;
+        }
+        System.out.println(somaPop);
+        System.out.println(cont);
+        System.out.println("Média da população: " + (somaPop / cont));
+
 
     }
+
 
 }
--- TesteJavaMySQL_T2/src/main/Main.java
+++ TesteJavaMySQL_T2/src/main/Main.java
@@ -8,15 +8,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException, SQLException {
 
+        //1º criar a conexão
         MYSQLConnection connection = new MYSQLConnection();
 
-        ResultSet result = connection.querySELECT("SELECT Name FROM city");
+        //2º adicionar uma linha
+        //connection.insertLineCity("Angra do Heroísmo", "PRT", "Açores", 25000);
+
+        //2º eliminar a linha 4083
+        //connection.deleteFromCity(4083);
+        //3º passo mostra os nomes das cidades
+        //ResultSet result = connection.querySELECT("SELECT ID, Name FROM city");
         //mostrar os nomes de todas as cidades
-        while(result.next()) {
+        /*while(result.next()) {
+            int id = result.getInt("ID");
+            System.out.print(id + " ; ");
             String area = result.getString("Name");
             System.out.println(area);
-        }
+        }*/
 
+        connection.mediaPopul();
     }
 
 }
