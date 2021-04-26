ff13a8d43a0655ac8a14228e7675c2bff6d89cb5
dcota <duartenunocota@gmail.com>
2021-04-26 11:04:40 +0000

    update

---
+++ JavaFX_demo/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    Button btnMudaTexto;
+
+    @FXML
+    TextField tfOrigem;
+
+    @FXML
+    TextField tfDestino;
+
+
+}
