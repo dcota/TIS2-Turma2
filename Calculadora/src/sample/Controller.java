42f1010f135c6d56b642767b94252f6bc3c3c51c
dcota <duartenunocota@gmail.com>
2021-05-06 19:58:10 +0000

    update

--- Calculadora/src/sample/Controller.java
+++ Calculadora/src/sample/Controller.java
@@ -2,10 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
 public class Controller {
 
@@ -17,6 +14,7 @@ public class Controller {
     @FXML RadioButton rbMult;
     @FXML RadioButton rbDiv;
     @FXML Button btnCalcular;
+    @FXML Button btnLimpar;
 
     private ToggleGroup tg = new ToggleGroup();
 
@@ -26,17 +24,81 @@ public class Controller {
         this.rbSoma.setToggleGroup(tg);
         this.rbSubtr.setToggleGroup(tg);
         this.rbMult.setToggleGroup(tg);
-
     }
 
     public void calcular(ActionEvent event) {
-        //ir buscar os valores às caixas de texto Num1 e Num2
-        String num1 = this.tfNum1.getText();
-        String num2 = this.tfNum2.getText();
-        //converter os valores de string para double
-        double n1 = Double.parseDouble(num1);
-        double n2 = Double.parseDouble(num2);
-        //
+        try {
+            //ir buscar os valores às caixas de texto Num1 e Num2
+            String num1 = this.tfNum1.getText();
+            String num2 = this.tfNum2.getText();
+            //converter os valores de string para double
+            double n1 = 0;
+            double n2 = 0;
+            if (num1.isEmpty() || num2.isEmpty()) {
+                alertaErro("Os valores não podem estar vazios!");
+            }
+            else {
+                n1 = Double.parseDouble(num1);
+                n2 = Double.parseDouble(num2);
+                double resultado = 0;
+                //verificar qual foi a operação escolhida
+                if (this.rbSoma.isSelected()) {
+                    resultado = n1 + n2;
+                    this.tfResultado.setText(String.valueOf(resultado));
+                    alertaSucesso("Soma");
+                    limpar();
+                } else if (this.rbSubtr.isSelected()) {
+                    resultado = n1 - n2;
+                    this.tfResultado.setText(String.valueOf(resultado));
+                    alertaSucesso("Subtração");
+                    limpar();
+                } else if (this.rbMult.isSelected()) {
+                    resultado = n1 * n2;
+                    this.tfResultado.setText(String.valueOf(resultado));
+                    alertaSucesso("Multiplicação");
+                    limpar();
+                } else if (this.rbDiv.isSelected()) {
+                    if (n2!=0) {
+                        resultado = (double) n1 / n2;
+                        this.tfResultado.setText(String.valueOf(resultado));
+                        alertaSucesso("Divisão");
+                        limpar();
+                    }
+                    else {
+                        alertaErro("Divisão por zero!");
+                    }
+                }
+            }
+        }
+        catch (NumberFormatException e) {
+            alertaErro("Formato de números inválido! Verifique os valores...");
+        }
+    }
+
+    public void alertaSucesso (String texto) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("SUCESSO");
+        alerta.setHeaderText(null);
+        alerta.setContentText(texto + " efetuada com sucesso!");
+        alerta.showAndWait();
+    }
+
+    public void alertaErro (String texto) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("ERRO");
+        alerta.setHeaderText(null);
+        alerta.setContentText(texto);
+        alerta.showAndWait();
+    }
+
+    public void limpar() {
+        this.tfNum1.clear();
+        this.tfNum2.clear();
+        this.tfResultado.clear();
+        this.rbDiv.setSelected(false);
+        this.rbSubtr.setSelected(false);
+        this.rbSoma.setSelected(false);
+        this.rbMult.setSelected(false);
     }
 
 }
